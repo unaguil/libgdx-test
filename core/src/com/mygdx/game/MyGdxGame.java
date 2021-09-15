7ce9fb22d7f648c4ee639c3e13faaed009333556
Unai Aguilera <unai.aguilera@deusto.es>
2021-09-15 15:26:25 +0200

    Añadido dibujado del mapa

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,5 +1,9 @@
 package com.mygdx.game;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileReader;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -7,40 +11,94 @@ import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.Gdx;
 
 public class MyGdxGame extends ApplicationAdapter {
+
+	private static final String NOMBRE_APP = "Test app";
+	private static final int ANCHO_MAPA = 25;
+	private static final int ALTO_MAPA = 25;
+	private static final int TAM_BALDOSA = 16;
+
 	SpriteBatch batch;
-	Texture img;
+	Texture suelo;
+	Texture arbol;
+	Texture montana;
+
+	char[][] mapa = new char[ANCHO_MAPA][ALTO_MAPA];
 	
 	@Override
-	public void create () {
-		Gdx.app.log("Test App", "App creada");
+	public void create() {
+		Gdx.app.log(NOMBRE_APP, "App creada");
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		suelo = new Texture("suelo.png");
+		arbol = new Texture("arbol.png");
+		montana = new Texture("montana.png");
+
+		// cargamos el fichero en el array en memoria
+		try (BufferedReader reader = new BufferedReader(new FileReader("mapa.txt"))) {
+			String linea = null;
+
+			int contadorLinea = 0;
+			while ((linea = reader.readLine()) != null) {
+				mapa[contadorLinea] = linea.toCharArray();
+				contadorLinea++;
+			}
+		} catch (IOException e) {
+			Gdx.app.log(NOMBRE_APP, "No se ha podido cargar el fichero de mapa");
+		}
 	}
 
 	@Override
-	public void render () {
-		//Gdx.app.log("Test App", "renderizando...");
-		ScreenUtils.clear(1, 0, 0, 1);
+	public void render() {
+		ScreenUtils.clear(0, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+
+		// aqui vamos a dibujar en pantalla cada baldosa
+		// desde la esquina superior izquierda
+
+		for (int i = 0; i < ANCHO_MAPA; i++) {
+			for (int j = 0; j < ALTO_MAPA; j++) {
+				// posición actual de dibujado
+				// tenemos en cuenta que el eje y está invertido
+				// además, la esquina de dibujado de la imagen es la inferior izquierda
+				int x = j * TAM_BALDOSA; 
+				int y = Gdx.graphics.getHeight() - 1 - (TAM_BALDOSA * (i + 1)); 
+				
+				switch (mapa[i][j]) {
+					case '#': 	batch.draw(suelo, x, y);
+								break;
+
+					case 'M':	batch.draw(suelo, x, y);
+								batch.draw(montana, x, y);
+								break;
+
+					case 'A':	batch.draw(suelo, x, y);
+								batch.draw(arbol, x, y);
+								break;
+
+					default: 	break;
+				}
+			}
+		}
+		
 		batch.end();
 	}
 	
 	@Override
-	public void dispose () {
-		Gdx.app.log("Test App", "Deteniendo el app");
+	public void dispose() {
+		Gdx.app.log(NOMBRE_APP, "Deteniendo el app");
 		batch.dispose();
-		img.dispose();
+		suelo.dispose();
+		arbol.dispose();
+		montana.dispose();
 	}
 
 	@Override
 	public void resize(int width, int height) {
-		Gdx.app.log("Test App", String.format("Tamaño pantalla: %d x %d", width, height));
+		Gdx.app.log(NOMBRE_APP, String.format("Tamaño pantalla: %d x %d", width, height));
 	}
 
 	@Override
 	public void pause() {
-		Gdx.app.log("Test App", "App pausada");
+		Gdx.app.log(NOMBRE_APP, "App pausada");
 	}
 
 	@Override
