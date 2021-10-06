a55f5d3d9335b5cdd0d5818d34e629efcd087e2d
Unai Aguilera <unai.aguilera@deusto.es>
2021-10-06 19:39:32 +0200

    Uso de lwjgl3-backend para evitar un problema en Linux y algunas versiones de JDK

--- desktop/src/es/deusto/prog3/captureflag/desktop/DesktopLauncher.java
+++ desktop/src/es/deusto/prog3/captureflag/desktop/DesktopLauncher.java
@@ -1,20 +1,18 @@
 package es.deusto.prog3.captureflag.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.*;
 
 import es.deusto.prog3.captureflag.CaptureTheFlag;
 
 // esta clase es el lanzador específico para escritorio
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		
 		// establecemos el modo por defecto para la aplicación al inicio
-		config.width = CaptureTheFlag.DEFAULT_WIDTH;
-		config.height = CaptureTheFlag.DEFAULT_HEIGHT;
-		config.title = "Capture The Flag v0.1";
+		config.setWindowedMode(CaptureTheFlag.DEFAULT_WIDTH, CaptureTheFlag.DEFAULT_HEIGHT);
+		config.setTitle("Capture The Flag v0.1");
 		
-		new LwjglApplication(new CaptureTheFlag(), config);
+		new Lwjgl3Application(new CaptureTheFlag(), config);
 	}
 }
